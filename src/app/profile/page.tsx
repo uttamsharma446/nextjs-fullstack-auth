cee8aaf08c80a54c963f4b83a2c33cdaa015b3a9
uttamsharma446 <suttam446@gmail.com>
2023-08-11 09:25:35 +0530

    add middleware to protect private route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,30 @@
 "use client";
+import axios from "axios";
 import React from "react";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      const response = await axios.get(`api/users/logout`);
+      toast.success("logout successful");
+      router.push("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button
+        onClick={logout}
+        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Profile;
